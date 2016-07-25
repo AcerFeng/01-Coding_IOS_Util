07b295af3364a5c5f2b6447b00e934f7762c6f2b
AcerFeng <lanfeng@lanfengdeMac-mini.local>
2016-07-25 20:17:23 +0800

    初始化
    项目初始化

---
+++ 01-Coding_IOS_Util/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-Coding_IOS_Util
+//
+//  Created by lanfeng on 16/7/25.
+//  Copyright © 2016年 lanfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
